298b089f5559041084a53abc89707b18b096fe43
Julie J <jeannet.julie@gmail.com>
2022-03-19 14:25:04 +0100

    Entités de base et authentification utilisateurs pour commencer ce projet de basique

---
+++ src/Entity/Ingredients.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientsRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: IngredientsRepository::class)]
+class Ingredients
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
+    private $name;
+
+    #[ORM\Column(type: 'integer')]
+    #[Assert\LessThan(200)]
+    #[Assert\NotBlank]
+    
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
